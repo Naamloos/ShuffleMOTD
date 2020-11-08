d36e5a2ece2ab3ffc230124b3d09ebd872f14f43
Ryan de Jonge <naamloosdt@gmail.com>
2020-11-09 00:38:18 +0100

    Json MOTD stuff

---
+++ ObsidianPlugin/Motds.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShuffleMOTD
+{
+    public class MotdCollection
+    {
+        [JsonProperty("MOTDs")]
+        public string[] Motds = { "Hi!", "Nice meme!" };
+
+        [JsonProperty("Format")]
+        public string Format = "Sample format! {0} is the motd";
+    }
+}
--- ObsidianPlugin/Plugin.cs
+++ ObsidianPlugin/Plugin.cs
@@ -1,4 +1,5 @@
-﻿using Obsidian.API;
+﻿using Newtonsoft.Json;
+using Obsidian.API;
 using Obsidian.API.Events;
 using Obsidian.API.Plugins;
 using Obsidian.API.Plugins.Services;
@@ -19,24 +20,34 @@ namespace ShuffleMOTD
         [Inject] public IFileReader FileReader { get; set; }
         [Inject] public IFileWriter FileWriter { get; set; }
 
+        MotdCollection motds;
+
         // One of server messages, called when an event occurs
         public async Task OnLoad(IServer server)
         {
             Logger.Log($"Loaded {Info.Name}.");
-            if (!FileWriter.FileExists("motds.txt"))
+
+            if (!FileWriter.FileExists("motd.json"))
             {
-                FileWriter.CreateFile("motds.txt");
-                FileWriter.WriteAllText("motds.txt", "ShuffleMOTD 1\nShuffleMOTD 2");
+                FileWriter.CreateFile("motd.json");
+
+                await FileWriter.WriteAllTextAsync("motd.json", JsonConvert.SerializeObject(new MotdCollection()));
+                Logger.Log($"Created new motds.json. Please edit this file and restart the server.");
             }
-            Logger.Log($"Created new motds.txt");
+
+            motds = JsonConvert.DeserializeObject<MotdCollection>(FileReader.ReadAllText("motd.json"));
+
             await Task.CompletedTask;
         }
 
         public async Task OnServerStatusRequest(ServerStatusRequestEventArgs args)
         {
-            var motds = FileReader.ReadAllLines("motds.txt");
+            if (motds.Motds.Length < 1)
+                return;
 
-            args.Status.Description.Text = motds[new Random().Next(0, motds.Length)].Replace("\\n", "\n");
+            var motd = string.Format(motds.Format, motds.Motds[new Random().Next(0, motds.Motds.Length)]);
+
+            args.Status.Description.Text = motd;
             await Task.CompletedTask;
         }
     }
