14655ca2fba80364bcdb016871d02f31dfe7ed04
Ryan de Jonge <naamloosdt@gmail.com>
2020-11-08 22:16:26 +0100

    ShuffleMOTD

--- ObsidianPlugin/Plugin.cs
+++ ObsidianPlugin/Plugin.cs
@@ -1,11 +1,13 @@
 ﻿using Obsidian.API;
+using Obsidian.API.Events;
 using Obsidian.API.Plugins;
 using Obsidian.API.Plugins.Services;
 using Obsidian.CommandFramework.Attributes;
 using Obsidian.CommandFramework.Entities;
+using System;
 using System.Threading.Tasks;
 
-namespace ObsidianPlugin
+namespace ShuffleMOTD
 {
     [Plugin(Name = "ShuffleMOTD", Version = "1.0",
             Authors = "Naamloos", Description = "Shuffles the MOTD",
@@ -14,31 +16,28 @@ namespace ObsidianPlugin
     {
         // Any interface from Obsidian.Plugins.Services can be injected into properties
         [Inject] public ILogger Logger { get; set; }
+        [Inject] public IFileReader FileReader { get; set; }
+        [Inject] public IFileWriter FileWriter { get; set; }
 
         // One of server messages, called when an event occurs
         public async Task OnLoad(IServer server)
         {
             Logger.Log($"Loaded {Info.Name}.");
-            server.RegisterCommandClass<PluginCommands>();
+            if (!FileWriter.FileExists("motds.txt"))
+            {
+                FileWriter.CreateFile("motds.txt");
+                FileWriter.WriteAllText("motds.txt", "ShuffleMOTD 1\nShuffleMOTD 2");
+            }
+            Logger.Log($"Created new motds.txt");
             await Task.CompletedTask;
         }
 
-        public class PluginCommands : BaseCommandClass
+        public async Task OnServerStatusRequest(ServerStatusRequestEventArgs args)
         {
-            [Command("click")]
-            [CommandInfo("Posts a clickable link to chat", "/health")]
-            public async Task HealthAsync(ObsidianContext ctx, [Remaining]string link)
-            {
-                var chat = IChatMessage.CreateNew();
-                chat.Text = $"<{ctx.Player.Username}>: {link}";
-                chat.ClickEvent.Action = ETextAction.OpenUrl;
-                chat.ClickEvent.Value = link;
-                chat.HoverEvent.Action = ETextAction.ShowText;
-                chat.HoverEvent.Value = "Click to open link";
-                chat.Underline = true;
+            var motds = FileReader.ReadAllLines("motds.txt");
 
-                await ctx.Player.SendMessageAsync(ctx.Player.Health.ToString());
-            }
+            args.Status.Description.Text = motds[new Random().Next(0, motds.Length)].Replace("\\n", "\n");
+            await Task.CompletedTask;
         }
     }
 }
